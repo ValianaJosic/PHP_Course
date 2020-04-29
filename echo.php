78f64a640c0fb997ef3dd47675de2c2fbff86643
Valiana <valiana.josic@gmail.com>
2020-04-29 17:04:56 -0400

    Comment PHP

---
+++ echo.php
@@ -0,0 +1,30 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php 
+    
+    $name = "Vali";
+    
+    echo "My name is {$name}";
+    print "Hello" . "World";
+
+  //Creating variable of my age      
+    $age = 29;
+
+    echo "My name is Valiana";
+    //echo "I am From Croatia";
+    #echo "I love Coffee";
+
+    
+   /* echo "My name is {$name}";
+    print "Hello" . "World";
+
+    $age = 24;*/
+
+?>
+</body>
+</html>
